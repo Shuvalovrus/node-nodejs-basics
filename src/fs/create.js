1f1ae9c15922c60db7d967eb3bd030a4dce6a637
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-02 16:12:50 +0300

    feat: add solution create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,12 @@
-export const create = async () => {
-    // Write your code here 
-};
+import { appendFile, open } from 'fs'
+
+export const create = (async () => {
+    await open('files/fresh.txt', (err) => {
+        if (!err) throw 'FS operation failed';
+        appendFile('files/fresh.txt', 'I am fresh and young', (err) => {
+            if (err) throw err;
+        })
+    })
+})();
+
+
