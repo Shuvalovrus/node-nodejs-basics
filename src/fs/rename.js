822255691184e32a30c912bce0e2147b2d108a6f
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-04 09:29:03 +0300

    feat: add rename task solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,7 @@
-export const rename = async () => {
-    // Write your code here 
-};
+import { rename as renameFile } from "fs"
+
+export const rename = (async () => {
+   renameFile('./files/wrongFilename.txt','./files/properFilename.md', (err) => {
+       if (err) throw 'FS operation failed'
+   })
+})();
