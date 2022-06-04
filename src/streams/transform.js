4625460018c7f2e66b2f702477b2e4a497cc832c
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-04 19:22:48 +0300

    feat: add solution for subtask Streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,27 @@
-export const transform = async () => {
-    // Write your code here 
-};
+import { Transform, pipeline } from "stream"
+
+export const transform = (async () => {
+    let readStream = process.stdin;
+    let writeStream = process.stdout;
+
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            let stringChunk = chunk.toString().trim();
+
+            this.push(stringChunk.split('').reverse().join('') + '\n');
+
+            callback()
+        }
+    })
+
+    pipeline(
+        readStream,
+        transformStream,
+        writeStream,
+        err => {
+            throw err
+        }
+
+    )
+})();
+
