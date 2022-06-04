8c32da802ab892bc8b38323a47e1c7e8ffac9380
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-04 18:46:49 +0300

    feat: add solution for subtask Streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,8 @@
-export const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream } from "fs";
+
+export const write = (async () => {
+    let stream = createWriteStream('./files/fileToWrite.txt');
+    process.stdin.on('data', (data) => {
+        stream.write(data);
+    })
+})();
