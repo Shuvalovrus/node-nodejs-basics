8e8e4f12395890c03c389234e64d935fc343f240
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-04 21:00:51 +0300

    feat: add Zlib/decompress subtask solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,21 @@
-export const decompress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from 'zlib'
+import { pipeline } from "stream"
+
+export const decompress = (async () => {
+    const readStream = createReadStream('./files/archive.gz');
+    const writeStream = createWriteStream('./files/fileToCompress.txt');
+
+    const transform = createUnzip()
+
+    pipeline(
+        readStream,
+        transform,
+        writeStream,
+        (err) => {
+            if (err) throw err;
+        }
+    )
+
+})();
+
