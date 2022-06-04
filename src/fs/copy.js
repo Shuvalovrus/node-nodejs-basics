e76f73f2a22dd75fe0777ef46d2490f517dd9787
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-04 09:17:20 +0300

    feat: add copy task solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,16 @@
-export const copy = async () => {
-    // Write your code here 
-};
+import { mkdir, readdir, copyFileSync } from 'fs'
+
+export const copy = (async () => {
+    mkdir('./files_copy', (err) => {
+        if (err) throw 'FS operation failed';
+
+        readdir('./files',(err,data) => {
+            if (err) throw 'FS operation failed';
+
+            data.forEach((item) => {
+                copyFileSync(`./files/${item}`, `./files_copy/${item}`)
+            })
+        });
+    });
+
+})();
