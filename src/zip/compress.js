bb800085e4724fc745ba67935782e40fd43da3a7
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-04 20:20:16 +0300

    feat: add solution for subtask Zlib/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,20 @@
-export const compress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from 'zlib'
+import { pipeline } from "stream"
+
+export const compress = (async () => {
+    const readStream = createReadStream('./files/fileToCompress.txt');
+    const writeStream = createWriteStream('./files/archive.gz');
+
+    const transform = createGzip()
+
+    pipeline(
+        readStream,
+        transform,
+        writeStream,
+        (err) => {
+            if (err) throw err;
+        }
+    )
+})();
+
