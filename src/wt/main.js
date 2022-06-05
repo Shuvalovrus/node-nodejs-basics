61e5c3c2583604884d5f7034e024c7dd4089551e
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-05 11:22:24 +0300

    feat: add solution task Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,24 @@
+import { cpus } from 'os'
+import { Worker } from 'worker_threads'
+
+console.log(10 + cpus().length);
 export const performCalculations = async () => {
-    // Write your code here
+    let arr = []
+    for (let i = 10; i < 10 + cpus().length; i++) {
+        arr.push(new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js', {
+                workerData : i
+            })
+            worker.on('message', message => {
+                resolve(message)
+            })
+            worker.on('error', err => {
+                resolve({ status: 'error', data: null })
+            })
+        }))
+    }
+    Promise.all(arr).then(value => console.log(value));
+
 };
+
+performCalculations();
