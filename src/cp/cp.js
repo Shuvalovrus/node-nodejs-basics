fa1dff82b9bb19c73b7bcf9dcbfb1cbd6580d6c0
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-05 13:52:16 +0300

    feat: add solution task Child Processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,17 @@
+import { fork } from 'child_process';
+const mainArg = process.argv.slice(2);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    let child = fork('./files/script.js', args, { stdio: ['pipe', 'pipe', 'pipe', 'ipc']})
+
+    child.stdout.on('data', (data) => {
+        process.stdout.write(`Received from child process: ${data}`)
+    });
+
+    process.stdin.on('data',(data) => {
+        child.stdin.write(data);
+    })
+
 };
+
+spawnChildProcess(mainArg)
